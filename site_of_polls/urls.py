fe22033bed41d7257f575083e15bda5984fbdc4b
Riccardo <riccardo_simi@yahoo.it>
2015-04-04 16:08:05 +0200

    modified to use generic views

--- site_of_polls/urls.py
+++ site_of_polls/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'site_of_polls.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
