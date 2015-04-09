8b9fc730b495b690492dd2c704108f8c9b7d205b
Riccardo <riccardo_simi@yahoo.it>
2015-04-09 14:39:59 +0200

    Added some static files, ended the tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -8,7 +8,7 @@ from polls import views
 
 urlpatterns = patterns('',
     # ex: /polls/
-    url(r'^$', views.IndexView.as_view/(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results
